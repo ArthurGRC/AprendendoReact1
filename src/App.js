66dce54649a65e5bcc626dc229bc0f027e293e45
Arthur <arthurzinugf@gmail.com>
2023-05-31 10:29:01 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import AddHello from './components/helloWorld.jsx';
+
+//JSX nada mais é do que um "JS" dentro do html, porém para utiliza-lo temos de usar todas as tags certinho
 
 function App() {
+
+  const name = "Arthur";
+
+  function sum(a,b){
+    return a + b;
+  }
+
+  
+  const url = 'https://via.placeholder.com/150'
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Olá {name}</h1>
+     <p>Meu primeiro app</p>
+     <p>soma = {sum(1,2)}</p>
+     <img src = {url} alt = 'Minha img' />
+     <p>{AddHello()}</p>
     </div>
   );
 }
---
+++ src/components/helloWorld.jsx
@@ -0,0 +1,14 @@
+
+
+
+const AddHello = () => {
+    
+    return (
+        <div>
+            <h1>Meu primeiro component</h1>
+        </div>
+    )
+    
+}
+
+export default AddHello
